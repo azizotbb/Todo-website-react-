43b5454c5c17df5ccf6a24343c62732f7eb5c20e
azizotbb <abdulaziz.otbb@gmail.com>
2024-06-23 18:51:10 +0300

    finish post and get

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://127.0.0.1:27017/myapp")
+  .connect("mongodb://localhost:27017/myapp")
   .then(() => {
     console.log("database is connected");
   })
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const mongoose = require("./db");
+const todo = require("./module/todo");
 const app = express();
-app.get("/hey", (req, res) => {
-  res.send("heyy vister");
+app.use(express.json());
+
+app.get("/data", async (req, res) => {
+  const todos = await todo.find();
+  res.send(todos);
+});
+app.post("/data", async (req, res) => {
+  await todo
+    .create(req.body)
+    .then(() => {
+      res.send("post successfully");
+    })
+    .catch((err) => {
+      console.log("there is an error !!!!!!!", err);
+    });
 });
 app.listen(3000, (req, res) => {
   console.log("server is working .....");
